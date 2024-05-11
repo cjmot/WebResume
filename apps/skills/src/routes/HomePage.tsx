9481a8ca009a16ff676a58c0104800e0196055d5
cjmot <cjmotyer@gmail.com>
2024-05-11 13:45:28 -0600

    Made routing buttons for each home and skills page

--- apps/skills/src/routes/HomePage.tsx
+++ apps/skills/src/routes/HomePage.tsx
@@ -1,10 +1,11 @@
-
+import {Link} from "react-router-dom";
 
 export default function SkillsRoutes() {
 
     return (
         <>
-            <h1 className='text-2xl font-semibold'>Welcome to skills</h1>
+            <h1 className='text-2xl font-semibold'>Welcome to Skills</h1>
+            <Link to='/' className='underline underline-offset-2 text-blue-500'>Home</Link>
         </>
     )
 }
